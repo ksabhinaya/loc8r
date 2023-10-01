e3972b04e85ad82abe5ed7971e79bf20f74ba8ae
unknown <21eg107a18@anurag.edu.in>
2023-10-01 20:59:27 +0530

    First commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,17 @@
+var express=require('express');
+var router = express.Router();
+var ctrlLocations=require('../controllers/locations.js');
+
+var ctrlOthers=require('../controllers/others.js'); 
+
+/* Locations pages */
+
+router.get('/', ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+
+router.get('/about', ctrlOthers.about);
+
+module.exports=router;
