4dbb29050a38a5b8db13609c283446003ae3bb0b
unknown <ksabhinaya>
2023-10-15 22:31:38 +0530

    mongodb commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var routesApi =require('./app_api/routes/index');
+require('./app_server/models/db');
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api',routesApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
